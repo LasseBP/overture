05fb8faca7dfd74374907cb48a106b19ef5284c9
Lasse Brøsted Pedersen <lasse@huhej.dk>
2015-10-21 17:07:17 +0200

    - Multiple fixes for value semantics.  - Handle L-values of assignments.  - Handle undefinedExp.  - Handle classtypes + methods types.

--- core/codegen/rustgen/src/main/java/org/overture/codegen/vdm2rust/Transforms/ValueSemanticsTrans.java
+++ core/codegen/rustgen/src/main/java/org/overture/codegen/vdm2rust/Transforms/ValueSemanticsTrans.java
@@ -2,11 +2,16 @@ package org.overture.codegen.vdm2rust.Transforms;
 
 import org.overture.codegen.cgast.INode;
 import org.overture.codegen.cgast.SExpCG;
+import org.overture.codegen.cgast.STypeCG;
 import org.overture.codegen.cgast.analysis.AnalysisException;
 import org.overture.codegen.cgast.analysis.DepthFirstAnalysisAdaptor;
 import org.overture.codegen.cgast.expressions.AApplyExpCG;
 import org.overture.codegen.cgast.expressions.AFieldExpCG;
 import org.overture.codegen.cgast.expressions.ANewExpCG;
+import org.overture.codegen.cgast.expressions.AStaticVarExpCG;
+import org.overture.codegen.cgast.expressions.AUndefinedExpCG;
+import org.overture.codegen.cgast.statements.AAssignToExpStmCG;
+import org.overture.codegen.cgast.types.AClassTypeCG;
 import org.overture.codegen.cgast.types.AMethodTypeCG;
 import org.overture.codegen.cgast.types.ATokenBasicTypeCG;
 import org.overture.codegen.cgast.types.SBasicTypeCG;
@@ -17,10 +22,30 @@ public class ValueSemanticsTrans extends DepthFirstAnalysisAdaptor {
 	@Override
 	public void defaultInSExpCG(SExpCG node) throws AnalysisException {
 		INode parent = node.parent();
+		STypeCG type = node.getType();
+		boolean isStatic = node instanceof AStaticVarExpCG;
 		
-		if (parent instanceof AFieldExpCG || 
-			node instanceof ANewExpCG ||
-			(node.getType() instanceof SBasicTypeCG && !(node.getType() instanceof ATokenBasicTypeCG))){
+		if (parent instanceof AFieldExpCG) {
+			return;
+		}
+		
+		if (parent instanceof AAssignToExpStmCG)
+		{
+			AAssignToExpStmCG assignment = (AAssignToExpStmCG) parent;
+			if (assignment.getTarget() == node)
+			{
+				return;
+			}
+		}
+		
+		if (node instanceof AUndefinedExpCG ||
+			node instanceof ANewExpCG) {
+			return;
+		}
+		
+		if(type instanceof AMethodTypeCG ||
+		   type instanceof AClassTypeCG ||
+		   (!isStatic && type instanceof SBasicTypeCG && !(type instanceof ATokenBasicTypeCG))) {		
 			return;
 		}
 		
