0927fb2e2cfc40b1c189b9f18198ca424c9d11c4
Lasse Brøsted Pedersen <lasse@huhej.dk>
2015-10-22 16:26:54 +0200

    Fixed tuplefieldexp + seq + map enum exps.

--- core/codegen/rustgen/src/main/resources/samples/bind_specs.rs
+++ core/codegen/rustgen/src/main/resources/samples/bind_specs.rs
@@ -5,44 +5,40 @@ use codegen_runtime::*;
 pub struct bind_specs;
 impl bind_specs {
 /* operations */
-    pub fn letbest_1(&mut self) -> (u64, u64) {
-        let (x, y): (u64, u64) = cartesian_set!(set!{1, 2, 3}, set!{1, 2, 3})
+    pub fn letbest_1() -> (u64, u64) {
+        let (x, y): (u64, u64) = cartesian_set!(set!(1, 2, 3), set!(1, 2, 3))
                                      .be_such_that(|(x, y): (u64, u64)| -> bool { y > x });
         return (x, y);
 
     }
 
-    pub fn set_comp1(&mut self) -> Set<u64> {
-        return set!{1, 2, 3}.set_compr(|z: u64| -> bool { true }, |z: u64| -> u64 { z * 2 });
+    pub fn set_comp1() -> Set<u64> {
+        return set!(1, 2, 3).set_compr(|z: u64| -> bool { true }, |z: u64| -> u64 { z * 2 });
     }
 
-    pub fn set_comp2(&mut self) -> Set<u64> {
-        return cartesian_set!(set!{1, 2, 3}, set!{1, 2, 3})
+    pub fn set_comp2() -> Set<u64> {
+        return cartesian_set!(set!(1, 2, 3), set!(1, 2, 3))
                    .set_compr(|(z, y): (u64, u64)| -> bool { z == y },
                               |(z, y): (u64, u64)| -> u64 { z * y });
     }
 
-    pub fn map_comp1(&mut self) -> Map<u64, char> {
-        return cartesian_set!(set!{1, 2, 3}, set!{'c', 'k'})
+    pub fn map_comp1() -> Map<u64, char> {
+        return cartesian_set!(set!(1, 2, 3), set!('c', 'k'))
                    .map_compr(|(z, y): (u64, char)| -> bool { true },
                               |(z, y): (u64, char)| -> (u64, char) { (z, y) });
     }
 
-    pub fn seq_comp1(&mut self) -> Seq<u64> {
-        return set!{1, 2, 3}.seq_compr(|z: u64| -> bool { z > 1 }, |z: u64| -> u64 { z * 2 });
+    pub fn seq_comp1() -> Seq<u64> {
+        return set!(1, 2, 3).seq_compr(|z: u64| -> bool { z > 1 }, |z: u64| -> u64 { z * 2 });
     }
 
-    pub fn exists_1(&mut self) -> bool {
-        return cartesian_set!(set!{1, 2, 3}, set!{1, 2, 3})
+    pub fn exists_1() -> bool {
+        return cartesian_set!(set!(1, 2, 3), set!(1, 2, 3))
                    .exists(|(x, y): (u64, u64)| -> bool { x > y });
     }
 
-    pub fn exists1_1(&mut self) -> bool {
-        return set!{1, 2, 3}.exists1(|x: u64| -> bool { x > 2 });
-    }
-
-    pub fn forall_1(&mut self) -> bool {
-        return cartesian_set!(set!{1, 2, 3}, set!{4, 5})
+    pub fn forall_1() -> bool {
+        return cartesian_set!(set!(1, 2, 3), set!(4, 5))
                    .forall(|(x, y): (u64, u64)| -> bool { y > x });
     }
 
@@ -55,10 +51,13 @@ impl bind_specs {
 
 /* functions */
     pub fn letbestExp_1() -> (u64, u64) {
-        let (x, y): (u64, u64) = cartesian_set!(set!{1, 2, 3}, set!{1, 2, 3})
+        let (x, y): (u64, u64) = cartesian_set!(set!(1, 2, 3), set!(1, 2, 3))
                                      .be_such_that(|(x, y): (u64, u64)| -> bool { y > x });
         (x, y)
 
+    }
+    pub fn exists1_1() -> bool {
+        set!(1, 2, 3).exists1(|x: u64| -> bool { x > 2 })
     }
 	}
 
