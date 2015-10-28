2a4e49486ce13fa99e7505a173314cfe9cf949f2
Lasse Brøsted Pedersen <lasse@huhej.dk>
2015-10-28 15:00:59 +0100

    Removed duplicate constructors from the IR.

--- core/codegen/rustgen/src/main/java/org/overture/codegen/vdm2rust/Transforms/ConstructorTrans.java
+++ core/codegen/rustgen/src/main/java/org/overture/codegen/vdm2rust/Transforms/ConstructorTrans.java
@@ -1,5 +1,8 @@
 package org.overture.codegen.vdm2rust.Transforms;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.overture.codegen.cgast.SStmCG;
 import org.overture.codegen.cgast.analysis.AnalysisException;
 import org.overture.codegen.cgast.analysis.DepthFirstAnalysisAdaptor;
@@ -26,73 +29,98 @@ public class ConstructorTrans extends DepthFirstAnalysisAdaptor {
 	}
 	
 	@Override
-	public void caseAMethodDeclCG(AMethodDeclCG node) throws AnalysisException {
+	public void defaultInSClassDeclCG(SClassDeclCG node) throws AnalysisException {
+		List<AMethodDeclCG> constructors = node.getMethods().stream()
+												.filter(method -> method.getIsConstructor())
+												.collect(Collectors.toList());
 		
-		if(node.getIsConstructor())
-		{
-			/*
-			 * let mut instance: ClassT = ClassT::default();
-			 * [instance.cg_init_x(<args>)]
-			 * return instance  
-			 */
-			
+		AMethodDeclCG selectedConstructor = null;
+		if(constructors.size() > 1) {
+			selectedConstructor = constructors.stream()
+									.filter(method -> method.getFormalParams().size() > 0)
+									.findFirst()
+									.get();			
+			 
+			// Rename here to make sure this constructor does not compare equal to other constructors (nodes .equals is based on string repr).
 			// conventional name in Rust.
-			node.setName("new");
-			// the new function/constructor is an associated function.
-			node.setStatic(true);
-			
-			SClassDeclCG enclosingClass = node.getAncestor(SClassDeclCG.class);
-			SStmCG oldBody = node.getBody();
-			boolean hasCustomConstructor = oldBody instanceof APlainCallStmCG;
-			
-			AClassTypeCG type = new AClassTypeCG();
-			type.setName(enclosingClass.getName());
-			
-			ABlockStmCG newBody = new ABlockStmCG();
-			
-			AExplicitVarExpCG defaultFunc = new AExplicitVarExpCG();
-			defaultFunc.setName("default");
-			defaultFunc.setType(type.clone());
-			defaultFunc.setClassType(type.clone());			
-			
-			AApplyExpCG applyDefaultFunc = new AApplyExpCG();
-			applyDefaultFunc.setType(type.clone());
-			applyDefaultFunc.setRoot(defaultFunc);
-			
-			AIdentifierPatternCG objPattern = new AIdentifierPatternCG();
-			objPattern.setName("instance");
-			
-			AVarDeclCG classVar = new AVarDeclCG();
-			classVar.setFinal(!hasCustomConstructor);
-			classVar.setExp(applyDefaultFunc);
-			classVar.setPattern(objPattern);	
-			classVar.setType(type.clone());
-						
-			newBody.getLocalDefs().add(classVar);
-			
-			AIdentifierVarExpCG instanceVarExp = new AIdentifierVarExpCG();
-			instanceVarExp.setIsLocal(true);
-			instanceVarExp.setName("instance");
-			
-			if(hasCustomConstructor)
-			{
-				// we need to call the custom constructor in the new body too.
-				APlainCallStmCG cg_init_call_old = (APlainCallStmCG)oldBody;
-				
-				ACallObjectExpStmCG cg_init_call = new ACallObjectExpStmCG();
-				cg_init_call.setArgs(cg_init_call_old.getArgs());
-				cg_init_call.setFieldName(cg_init_call_old.getName());
-				cg_init_call.setType(new AVoidTypeCG());
-				cg_init_call.setObj(instanceVarExp.clone());
-				
-				newBody.getStatements().add(cg_init_call);
+			selectedConstructor.setName("new");
+		
+			constructors.remove(selectedConstructor);
+			// remove non-selected constructors.
+			for(AMethodDeclCG condemned : constructors) {
+				node.removeChild(condemned);
 			}
-			
-			AReturnStmCG returnStm = new AReturnStmCG();
-			returnStm.setExp(instanceVarExp.clone());
-			newBody.getStatements().add(returnStm);
-			
-			node.setBody(newBody);
+		} else {
+			selectedConstructor = constructors.get(0);
 		}
+		
+		transformConstructor(selectedConstructor);
+	}
+
+	protected void transformConstructor(AMethodDeclCG node) {
+		/*
+		 * let mut instance: ClassT = ClassT::default();
+		 * [instance.cg_init_x(<args>)]
+		 * return instance  
+		 */
+		
+		// the new function/constructor is an associated function.
+		node.setStatic(true);
+		
+		SClassDeclCG enclosingClass = node.getAncestor(SClassDeclCG.class);
+		SStmCG oldBody = node.getBody();
+		boolean hasCustomConstructor = oldBody instanceof APlainCallStmCG;
+		
+		AClassTypeCG type = new AClassTypeCG();
+		type.setName(enclosingClass.getName());
+		type.setOptional(false);
+		
+		ABlockStmCG newBody = new ABlockStmCG();
+		
+		AExplicitVarExpCG defaultFunc = new AExplicitVarExpCG();
+		defaultFunc.setName("default");
+		defaultFunc.setType(type.clone());
+		defaultFunc.setClassType(type.clone());			
+		
+		AApplyExpCG applyDefaultFunc = new AApplyExpCG();
+		applyDefaultFunc.setType(type.clone());
+		applyDefaultFunc.setRoot(defaultFunc);
+		
+		AIdentifierPatternCG objPattern = new AIdentifierPatternCG();
+		objPattern.setName("instance");
+		
+		AVarDeclCG classVar = new AVarDeclCG();
+		classVar.setFinal(!hasCustomConstructor);
+		classVar.setExp(applyDefaultFunc);
+		classVar.setPattern(objPattern);	
+		classVar.setType(type.clone());
+					
+		newBody.getLocalDefs().add(classVar);
+		
+		AIdentifierVarExpCG instanceVarExp = new AIdentifierVarExpCG();
+		instanceVarExp.setIsLocal(true);
+		instanceVarExp.setName("instance");
+		instanceVarExp.setType(type.clone());
+		instanceVarExp.setIsLambda(false);
+		
+		if(hasCustomConstructor)
+		{
+			// we need to call the custom constructor in the new body too.
+			APlainCallStmCG cg_init_call_old = (APlainCallStmCG)oldBody;
+			
+			ACallObjectExpStmCG cg_init_call = new ACallObjectExpStmCG();
+			cg_init_call.setArgs(cg_init_call_old.getArgs());
+			cg_init_call.setFieldName(cg_init_call_old.getName());
+			cg_init_call.setType(new AVoidTypeCG());
+			cg_init_call.setObj(instanceVarExp.clone());
+			
+			newBody.getStatements().add(cg_init_call);
+		}
+		
+		AReturnStmCG returnStm = new AReturnStmCG();
+		returnStm.setExp(instanceVarExp.clone());
+		newBody.getStatements().add(returnStm);
+		
+		node.setBody(newBody);
 	}
 }
