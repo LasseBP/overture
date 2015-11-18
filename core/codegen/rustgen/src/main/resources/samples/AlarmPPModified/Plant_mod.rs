a1895aadaf0808420b2c48e80af92540b088c6b9
Lasse Brøsted Pedersen <lasse@huhej.dk>
2015-11-18 12:37:37 +0100

    Update samples.

--- core/codegen/rustgen/src/main/resources/samples/AlarmPPModified/Plant_mod.rs
+++ core/codegen/rustgen/src/main/resources/samples/AlarmPPModified/Plant_mod.rs
@@ -2,18 +2,18 @@ use codegen_runtime::*;
 use quotes;
 use Test1_mod::Test1;
 
-/* types */
+// types
 pub type Period = Token;
 
 pub type StringSeq = Seq<char>;
 
- #[derive(PartialEq, Eq, Clone, Hash)]
+#[derive(PartialEq, Eq, Clone, Hash)]
 pub enum Qualification {
-		Ch0(quotes::Bio),
-		Ch1(quotes::Chem),
-		Ch2(quotes::Elec),
-		Ch3(quotes::Mech),
-	}
+    Ch0(quotes::Bio),
+    Ch1(quotes::Chem),
+    Ch2(quotes::Elec),
+    Ch3(quotes::Mech),
+}
 impl_union! { Qualification:
 				quotes::Bio as Qualification::Ch0, 
 				quotes::Chem as Qualification::Ch1, 
@@ -21,91 +21,128 @@ impl_union! { Qualification:
 			quotes::Mech as Qualification::Ch3 
 	}
 
- #[derive(PartialEq, Eq, Clone, Hash, Debug)]
+#[derive(PartialEq, Eq, Clone, Hash, Debug)]
 pub struct Alarm {
-		pub descr: StringSeq,
-		pub reqQuali: Qualification,
-	}
+    pub descr: StringSeq,
+    pub reqQuali: Qualification,
+}
 impl_record! { Alarm: descr as StringSeq,  reqQuali as Qualification }
 
- #[derive(PartialEq, Eq, Clone, Hash, Debug)]
+#[derive(PartialEq, Eq, Clone, Hash, Debug)]
 pub struct Expert {
-		pub quali: Set<Qualification>,
-	}
+    pub quali: Set<Qualification>,
+}
 impl_record! { Expert: quali as Set<Qualification> }
 
 #[derive(PartialEq, Eq, Clone, Hash, Debug)]
-pub struct Plant  {
-		 alarms: Set<Alarm>,
-		 schedule: Map<Period,Set<Expert>>,
-	}
+pub struct Plant {
+    alarms: Set<Alarm>,
+    schedule: Map<Period, Set<Expert>>,
+}
 
 impl Plant {
-/* operations */
-				pub fn ExpertToPage(&mut self, a: Alarm, p: Period) -> Expert {
-			let  expert: Expert = self.schedule.get(p.clone()).be_such_that(|expert: Expert| -> bool { expert.quali.in_set(a.reqQuali.clone()) } );
-			return expert.clone();
-	
-} 
-	
-				pub fn NumberOfExperts(&mut self, p: Period) -> u64 {
-	return self.schedule.get(p.clone()).card();
-} 
-	
-				pub fn ExpertIsOnDuty(&mut self, ex: Expert) -> Set<Period> {
-	return self.schedule.domain().set_compr(|p: Period| -> bool { self.schedule.get(p.clone()).in_set(ex.clone()) } , |p: Period| -> Period { p.clone() } );
-} 
-	
-				pub fn cg_init_Plant_1(&mut self, als: Set<Alarm>, sch: Map<Period,Set<Expert>>) -> () {
-				self.alarms = als.clone();
-		self.schedule = sch.clone();
-	
-} 
-	
-			pub fn new(als: Set<Alarm>, sch: Map<Period,Set<Expert>>) -> Plant {
-			let mut instance: Plant = Plant::default();
-			{
-		let  arg0: Set<Alarm> = als.clone();
-		let  arg1: Map<Period,Set<Expert>> = sch.clone();
-			instance.cg_init_Plant_1(arg0.clone(), arg1.clone());
-	}
+    // operations
+    pub fn ExpertToPage(&mut self, a: Alarm, p: Period) -> Expert {
+        assert!(self.pre_ExpertToPage(a.clone(), p.clone()));
+        {
+            let expert: Expert = self.schedule
+                                     .get(p.clone())
+                                     .be_such_that(|expert: Expert| -> bool {
+                                         expert.quali.in_set(a.reqQuali.clone())
+                                     });
+            return expert;
+        }
 
-		return instance;
-	
-} 
-	
-				pub fn AddExpertToSchedule(&mut self, p: Period, ex: Expert) -> () {
-			let  arg0: Period = p.clone();
-		let  arg1: Set<Expert> = if self.schedule.domain().in_set(p.clone()) { set!(ex.clone()).union(self.schedule.get(p.clone())) } else { set!(ex.clone()) };
-			self.schedule.insert(arg0.clone(), arg1.clone());
-	
-} 
-	
-				pub fn RemoveExpertFromSchedule(&mut self, p: Period, ex: Expert) -> () {
-			let  exs: Set<Expert> = self.schedule.get(p.clone());
-			self.schedule = if exs.card() == 1 { self.schedule.dom_restrict_by(set!(p.clone())) } else { self.schedule.ovrride(map!(p.clone() => set!(ex.clone()).difference(exs.clone()))) };
-	
-} 
-	
-		
-/* functions */
-		 fn PlantInv(als: Set<Alarm>, sch: Map<Period,Set<Expert>>) -> bool {
-	sch.domain().forall(|p: Period| -> bool { sch.get(p.clone()) != set!() } ) && als.forall(|a: Alarm| -> bool { sch.domain().forall(|p: Period| -> bool { sch.get(p.clone()).exists(|expert: Expert| -> bool { expert.quali.in_set(a.reqQuali.clone()) } ) } ) } )
-} 
+
+    }
+
+    pub fn NumberOfExperts(&mut self, p: Period) -> u64 {
+        assert!(self.pre_NumberOfExperts(p.clone()));
+        return self.schedule.get(p.clone()).card();
+
+    }
+
+    pub fn ExpertIsOnDuty(&mut self, ex: Expert) -> Set<Period> {
+        return self.schedule.domain().set_compr(|p: Period| -> bool {
+                                                    self.schedule.get(p.clone()).in_set(ex.clone())
+                                                },
+                                                |p: Period| -> Period { p.clone() });
+    }
+
+    pub fn new(als: Set<Alarm>, sch: Map<Period, Set<Expert>>) -> Plant {
+        assert!(self.pre_Plant(als.clone(), sch.clone()));
+        let instance: Plant = Plant::default();
+        return instance;
+
+
+    }
+
+    pub fn AddExpertToSchedule(&mut self, p: Period, ex: Expert) -> () {
+        let arg0: Period = p.clone();
+        let arg1: Set<Expert> = if self.schedule.domain().in_set(p.clone()) {
+            self.schedule.get(p.clone()).union(set!(ex.clone()))
+        } else {
+            set!(ex.clone())
+        };
+        self.schedule.insert(arg0.clone(), arg1.clone());
+
+    }
+
+    pub fn RemoveExpertFromSchedule(&mut self, p: Period, ex: Expert) -> () {
+        assert!(self.pre_RemoveExpertFromSchedule(p.clone(), ex.clone()));
+        let exs: Set<Expert> = self.schedule.get(p.clone());
+        self.schedule = if exs.card() == 1 {
+            self.schedule.dom_restrict_by(set!(p.clone()))
+        } else {
+            self.schedule.ovrride(map!(p.clone() => exs.difference(set!(ex.clone()))))
+        };
+
+
+    }
+
+    fn pre_ExpertToPage(&self, a: Alarm, p: Period) -> bool {
+        return self.alarms.in_set(a.clone()) && self.schedule.domain().in_set(p.clone());
+    }
+
+    fn pre_NumberOfExperts(&self, p: Period) -> bool {
+        return self.schedule.domain().in_set(p.clone());
+    }
+
+    fn pre_Plant(&self, als: Set<Alarm>, sch: Map<Period, Set<Expert>>) -> bool {
+        return Plant::PlantInv(als.clone(), sch.clone());
+    }
+
+    fn pre_RemoveExpertFromSchedule(&self, p: Period, ex: Expert) -> bool {
+        return self.schedule.domain().in_set(p.clone()) &&
+               self.alarms.forall(|a: Alarm| -> bool {
+            self.schedule
+                .get(p.clone())
+                .difference(set!(ex.clone()))
+                .exists(|expert: Expert| -> bool { expert.quali.in_set(a.reqQuali.clone()) })
+        });
+    }
+
+
+    // functions
+    fn PlantInv(als: Set<Alarm>, sch: Map<Period, Set<Expert>>) -> bool {
+        sch.domain().forall(|p: Period| -> bool { sch.get(p.clone()) != set!() }) &&
+        als.forall(|a: Alarm| -> bool {
+            sch.domain().forall(|p: Period| -> bool {
+                sch.get(p.clone())
+                   .exists(|expert: Expert| -> bool { expert.quali.in_set(a.reqQuali.clone()) })
+            })
+        })
+    }
 	}
 
 impl Default for Plant {
-	fn default() -> Plant {
-				let alarms: Set<Alarm> = Default::default();
-				let schedule: Map<Period,Set<Expert>> = Default::default();
-				
-		Plant
-				{
-						alarms: alarms,
-						schedule: schedule,
-					}
-			}
-}
-	
+    fn default() -> Plant {
+        let alarms: Set<Alarm> = Default::default();
+        let schedule: Map<Period, Set<Expert>> = Default::default();
 
-	
+        Plant {
+            alarms: alarms,
+            schedule: schedule,
+        }
+    }
+}
--- core/codegen/rustgen/src/main/resources/samples/AlarmPPModified/Test1_mod.rs
+++ core/codegen/rustgen/src/main/resources/samples/AlarmPPModified/Test1_mod.rs
@@ -2,8 +2,8 @@ use codegen_runtime::*;
 use quotes;
 use Plant_mod::Plant;
 
-/* types */
-/* values */
+// types
+// values
 lazy_static! {
 		 static ref p1: ::Plant_mod::Period = Token::new(&strseq!("Monday day"));
 		 static ref p2: ::Plant_mod::Period = Token::new(&strseq!("Monday night"));
@@ -12,57 +12,75 @@ lazy_static! {
 	}
 
 #[derive(PartialEq, Eq, Clone, Hash, Debug)]
-pub struct Test1  {
-		 a1: ::Plant_mod::Alarm,
-		 a2: ::Plant_mod::Alarm,
-		 ex1: ::Plant_mod::Expert,
-		 ex2: ::Plant_mod::Expert,
-		 ex3: ::Plant_mod::Expert,
-		 ex4: ::Plant_mod::Expert,
-		 plant: Plant,
-	}
+pub struct Test1 {
+    a1: ::Plant_mod::Alarm,
+    a2: ::Plant_mod::Alarm,
+    ex1: ::Plant_mod::Expert,
+    ex2: ::Plant_mod::Expert,
+    ex3: ::Plant_mod::Expert,
+    ex4: ::Plant_mod::Expert,
+    plant: Plant,
+}
 
 impl Test1 {
-/* operations */
-		pub fn Run(&mut self) -> (Set<::Plant_mod::Period>, ::Plant_mod::Expert) {
-			let  periods: Set<::Plant_mod::Period> = self.plant.ExpertIsOnDuty(self.ex1.clone());
-		let  expert: ::Plant_mod::Expert = self.plant.ExpertToPage(self.a1.clone(), ::Test1_mod::p1.clone());
-			return (periods.clone(), expert.clone());
-	
-} 
-	
-		pub fn new() -> Test1 {
-			let  instance: Test1 = Test1::default();
-			return instance;
-	
-} 
-	
-		
-/* functions */
+    // operations
+    pub fn Run(&mut self) -> (Set<::Plant_mod::Period>, ::Plant_mod::Expert) {
+        let periods: Set<::Plant_mod::Period> = self.plant.ExpertIsOnDuty(self.ex1.clone());
+        let expert: ::Plant_mod::Expert = self.plant.ExpertToPage(self.a1.clone(),
+                                                                  ::Test1_mod::p1.clone());
+        return (periods.clone(), expert.clone());
+
+    }
+
+    pub fn new() -> Test1 {
+        let instance: Test1 = Test1::default();
+        return instance;
+
+    }
+
+    pub fn preCond_test(i: i64) -> i64 {
+        assert!(Test1::pre_preCond_test(i));
+        return panic!("undefined");
+
+    }
+
+
+    // functions
+    pub fn pre_preCond_test(i: i64) -> bool {
+        i != 0
+    }
 	}
 
 impl Default for Test1 {
-	fn default() -> Test1 {
-				let a1: ::Plant_mod::Alarm = ::Plant_mod::Alarm::new(strseq!("Mechanical fault"), ::Plant_mod::Qualification::from(quotes::Mech));
-				let a2: ::Plant_mod::Alarm = ::Plant_mod::Alarm::new(strseq!("Tank overflow"), ::Plant_mod::Qualification::from(quotes::Chem));
-				let ex1: ::Plant_mod::Expert = ::Plant_mod::Expert::new(set!(::Plant_mod::Qualification::from(quotes::Mech), ::Plant_mod::Qualification::from(quotes::Bio)));
-				let ex2: ::Plant_mod::Expert = ::Plant_mod::Expert::new(set!(::Plant_mod::Qualification::from(quotes::Elec)));
-				let ex3: ::Plant_mod::Expert = ::Plant_mod::Expert::new(set!(::Plant_mod::Qualification::from(quotes::Chem), ::Plant_mod::Qualification::from(quotes::Bio), ::Plant_mod::Qualification::from(quotes::Mech)));
-				let ex4: ::Plant_mod::Expert = ::Plant_mod::Expert::new(set!(::Plant_mod::Qualification::from(quotes::Elec), ::Plant_mod::Qualification::from(quotes::Chem)));
-				let plant: Plant = Plant::new(set!(a1.clone()), map!(::Test1_mod::p1.clone() => set!(ex1.clone(), ex4.clone()), ::Test1_mod::p2.clone() => set!(ex2.clone(), ex3.clone())));
-				
-		Test1
-				{
-						a1: a1,
-						a2: a2,
-						ex1: ex1,
-						ex2: ex2,
-						ex3: ex3,
-						ex4: ex4,
-						plant: plant,
-					}
-			}
-}
-	
+    fn default() -> Test1 {
+        let a1: ::Plant_mod::Alarm =
+            ::Plant_mod::Alarm::new(strseq!("Mechanical fault"),
+                                    ::Plant_mod::Qualification::from(quotes::Mech));
+        let a2: ::Plant_mod::Alarm =
+            ::Plant_mod::Alarm::new(strseq!("Tank overflow"),
+                                    ::Plant_mod::Qualification::from(quotes::Chem));
+        let ex1: ::Plant_mod::Expert =
+            ::Plant_mod::Expert::new(set!(::Plant_mod::Qualification::from(quotes::Mech),
+                                          ::Plant_mod::Qualification::from(quotes::Bio)));
+        let ex2: ::Plant_mod::Expert =
+            ::Plant_mod::Expert::new(set!(::Plant_mod::Qualification::from(quotes::Elec)));
+        let ex3: ::Plant_mod::Expert =
+            ::Plant_mod::Expert::new(set!(::Plant_mod::Qualification::from(quotes::Chem),
+                                          ::Plant_mod::Qualification::from(quotes::Bio),
+                                          ::Plant_mod::Qualification::from(quotes::Mech)));
+        let ex4: ::Plant_mod::Expert =
+            ::Plant_mod::Expert::new(set!(::Plant_mod::Qualification::from(quotes::Elec),
+                                          ::Plant_mod::Qualification::from(quotes::Chem)));
+        let plant: Plant = Plant::new(set!(a1.clone()), map!(::Test1_mod::p1.clone() => set!(ex1.clone(), ex4.clone()), ::Test1_mod::p2.clone() => set!(ex2.clone(), ex3.clone())));
 
-	
+        Test1 {
+            a1: a1,
+            a2: a2,
+            ex1: ex1,
+            ex2: ex2,
+            ex3: ex3,
+            ex4: ex4,
+            plant: plant,
+        }
+    }
+}
